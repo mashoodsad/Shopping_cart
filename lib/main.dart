2568864a572198b65e3e5407d2e76139ce785a08
saad <mashoodsad@gmail.com>
2023-02-09 22:22:45 +0530

    cart page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:samplejsonbloc/Data.dart';
 import 'package:samplejsonbloc/Product.dart';
 
+import 'Categories.dart';
+import 'DetailsScreen.dart';
 import 'bloc_user_bloc.dart';
 
+import 'generated/constants.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -14,6 +18,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'shopping cart',
+      theme: ThemeData(textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity),
       home: HomePage(),
     );
   }
@@ -35,14 +43,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Product')),
-      body: _buildListCovid(),
+      appBar: buildAppBar(),
+      body: _buildListProduct(),
     );
   }
 
-  Widget _buildListCovid() {
+  Widget _buildListProduct() {
     return Container(
-      margin: EdgeInsets.all(8.0),
+      margin: const EdgeInsets.all(8.0),
       child: BlocProvider(
         create: (_) => _newsBloc,
         child: BlocListener<UserBloc, ProductState>(
@@ -76,34 +84,99 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildCard(BuildContext context, Product model) {
-    return ListView.builder(
-      itemCount: model.data!.length,
-      itemBuilder: (context, index) {
-        var s=model.data[index] as Data;
-        return Container(
-          margin: EdgeInsets.all(8.0),
-          child: Card(
-            child: Container(
-              margin: EdgeInsets.all(8.0),
-              child: Column(
-                children: <Widget>[
-                  Text("Status: ${model.status}"),
-                  Text(
-                      "name: ${s.name}"),
-                  Text("Brand: ${s.brand}"),
-                  Image.network('${s.image}'),
-                  Text(
-                      "price: ${s.price}"),
-                  Text("Brand: ${s.description}"),
+    return Column(
+      children: [Align(alignment: Alignment.topLeft,
+        child: Text('Shopping', style: Theme.of(context)
+          .textTheme
+          .headline5
+          ?.copyWith(fontWeight: FontWeight.bold),),
+      ),
+        Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+            child: GridView.builder(
+                itemCount: model.data!.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: kDefaultPaddin,
+                  crossAxisSpacing: kDefaultPaddin,
+                  childAspectRatio: 0.75,
+                ),
+              itemBuilder: (context, index) {
+                var s=model.data[index] as Data;
+                return GestureDetector(
+                  onTap: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                        builder: (context) => DetailsScreen()
+                    )
+                    );
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Container(
+                        margin: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: <Widget>[
 
-                ],
-              ),
+                            Image.network('${s.image}'),
+                            Text(
+                                "name: ${s.name}"),
+                            Text("Brand: ${s.brand}"),
+                            Text(
+                                "price: Rs${s.price}"),
+
+
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 
   Widget _buildLoading() => Center(child: CircularProgressIndicator());
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/back.svg"),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            // By default our  icon color is white
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            // By default our  icon color is white
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        SizedBox(width: kDefaultPaddin / 2)
+      ],
+    );
+  }
 }
+const kTextColor = Color(0xFF535353);
+const kTextLightColor = Color(0xFFACACAC);
+
+const kDefaultPaddin = 20.0;
